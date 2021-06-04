c6fd0f55aa71bcdcf036233314ecdc7d3464378e
William Horning <williamhorning@wgyt.tk>
2021-06-04 14:21:41 +0000

    screw frontends

--- index.js
+++ index.js
@@ -1,59 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
-require('dotenv').config();
-const jsoning = require('jsoning');
+require("dotenv").config();
+const jsoning = require("jsoning");
 var status = new jsoning("status.json");
 var auth = new jsoning("auth.json");
 const fetch = require("node-fetch");
 const retronid = require("retronid");
-// home page
-app.get('/', (req, res) => {
-	res.sendFile("/home/runner/onestate-server/pages/index.html");
-});
-// css
-app.get('/css.css', (req, res) => {
-	res.sendFile("/home/runner/onestate-server/css/tailwind.css");
-});
 // auth pages
-app.get('/auth', (req, res) => {
-	res.sendFile("/home/runner/onestate-server/pages/welcomeauth.html");
-});
-app.get('/auth/send', (req, res) => {
-	res.redirect(`https://fluffyscratch.hampton.pw/auth/getKeys/v2?redirect=aXNvbi53Z3l0LnRrL2F1dGgvY2FsbGJhY2s`);
+app.get("/auth", (req, res) => {
+	res.redirect(`https://scratch.auth.onedot.cf/?url=https://state.onedot.cf/auth/callback`);
 });
 app.get("/auth/callback", (req, res) => {
 	var response;
-	h = fetch(`https://fluffyscratch.hampton.pw/auth/verify/v2/${req.query.privateCode}`).then((response) => {
-		if (response.valid === true) {
-			retroid = retronid.generate();
-			res.json({
-				"user": req.query.username,
-				"retronid": retroid
-			});
-			status.set(`${req.query.username}`,false);
-			auth.set(`${req.query.username}`,retroid);
-		} else {
-			res.redirect('/auth');
-		}
-	});
-});
-app.post('/api/v1/:user', async(req, res) => {
-	authtoken = req.body.retroid;
-	username = req.params.user;
-	if (await auth.get(req.params.user) === authtoken) {
-		status.set(`${req.params.user}`,req.body.online||false);
-		res.send('done');
+	if (req.query.verified == 'true') {
+		retroid = retronid.generate();
+		res.send(`<h1>Welcome @${req.query.username}! Enter the following code in your client:</h1><br><h2>${retroid}</h2>`)
+		status.set(`${req.query.username}`,true);
+		auth.set(`${req.query.username}`, retroid);
 	} else {
-		res.send('error');
-		console.log('auth error')
+		res.redirect("/auth");
 	}
 });
-app.listen(3000);
-app.get('/api/v1/:user', async (req, res) => {
-	json = await (await fetch(`https://my-ocular.jeffalo.net/api/user/${req.params.user}`)).json()
-	data = {status:await status.get(req.params.user),ocular:json}
-	res.json(data)
+// api
+app.post("/api/v1/user/:user", async (req, res) => {
+	authtoken = req.body.retroid;
+	username = req.params.user;
+	if ((await auth.get(req.params.user)) === authtoken) {
+		status.set(`${req.params.user}`, req.body.online || false);
+		res.json({"ok":true});
+	} else {
+		res.json({"ok":false});
+		console.log("auth error");
+	}
 });
+app.get("/api/v1/user/:user", async (req, res) => {
+	json = await (await fetch(`https://my-ocular.jeffalo.net/api/user/${req.params.user}`)).json();
+	data = { status: await status.get(req.params.user), ocular: json };
+	res.json(data);
+});
+app.get("/api/v1/users/", async (req, res) => {
+	res.json(await status.all());
+});
+app.listen(3000);
